ba61fbde060ced55e89a0bfd4e4230269425e127
laiwentao1987 <wentao9698@gmail.com>
2025-09-23 23:14:25 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,26 +3,9 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+    
  };       
 
-    dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-        name: 'TV-电影天堂',
-    }
- };       
-
-    ruyi: {
-        api: 'https://cj.rycjapi.com/api.php/provide/vod',
-        name: '如意资源',
-    }
- };       
-
-    souavzy: {
-        api: 'https://api.souavzy.vip/api.php/provide/vod',
-        name: 'souavZY',
-    }
-   
-};
 
 // 调用全局方法合并
 if (window.extendAPISites) {
