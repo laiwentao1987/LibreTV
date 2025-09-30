8aca5eb9ff24c900cf163898a81779a9fd3e7c83
laiwentao1987 <wentao9698@gmail.com>
2025-09-30 21:40:49 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,23 +3,25 @@ const CUSTOMER_SITES = {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂',
     }
-  const CUSTOMER_SITES = {
-    dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-        name: '电影天堂',
-    }
-    const CUSTOMER_SITES = {
-    dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-        name: '电影天堂',
-    }
-    const CUSTOMER_SITES = {
-    dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-        name: '电影天堂',
-    }
  };       
-
+const CUSTOMER_SITES = {
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂',
+    }
+ };   
+const CUSTOMER_SITES = {
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂',
+    }
+ };   
+const CUSTOMER_SITES = {
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂',
+    }
+ };   
 
 // 调用全局方法合并
 if (window.extendAPISites) {
